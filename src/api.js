1e86ae557c109f42a7bc1d29ec8d8536868c1c54
monika-stempniak <mstempniak@gmail.com>
2019-02-04 22:49:29 +0100

    Write to file when download

--- src/api.js
+++ src/api.js
@@ -1,13 +1,21 @@
 const http = require('./http');
+const { writeFile } = require('fs');
 const { apiURL } = require('./constants');
 
 async function uploadTodolist(todolist) {
   const response = await http.post(apiURL, todolist);
+  
   return response.data;
 }
 
 async function downloadTodolist() {
   const response = await http.get(apiURL);
+  const todo = JSON.stringify(response.data);
+
+  writeFile('todo.json', todo, function (err) {
+    if (err) throw err;
+  });
+
   return response.data;
 }
 
